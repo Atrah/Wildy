8d6389e29940a27eb4b37fc8c394c8c17c4970a0
Alex <Alex@nullptr>
2019-02-17 11:01:45 -0500

    ??

--- Atrah_WildyLooter2.0/src/core/Main.java
+++ Atrah_WildyLooter2.0/src/core/Main.java
@@ -1,60 +1,93 @@
-package core;
+package script;
 
-import org.osbot.rs07.api.map.Area;
-import org.osbot.rs07.randoms.WelcomeScreen;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
-
-import script.TutorialIsland;
+import sections.*;
 import src.Looter;
 
-@ScriptManifest(name = "Atrah Wildy looter", author = "Atrahasis", info = "wildy looter", version = 2.0, logo = "")
-public class Main extends Script{
-	
-	private final Area TUTO_ISLAND = new Area (3045, 3138, 3157, 3057);
-	private boolean needATask = true;
-	
-	@Override
-	public void onStart() throws InterruptedException {
-		if(getClient().isLoggedIn()) {
-			getScript();
-		}
-	}
-	
-	@Override
-	public int onLoop() throws InterruptedException {
-		if (!getClient().isLoggedIn() || getWidgets().isVisible(WelcomeScreen.INTERFACE)) {
-            return random(1200, 1800);
-        } else if (getClient().isLoggedIn() && needATask == true) {
-        	getScript();
+@ScriptManifest(author = "Explv", name = "Explv's Tutorial Island", info = "Completes Tutorial Island", version = 6.0, logo = "")
+public final class TutorialIsland extends Script {
+
+    private final TutorialSection rsGuideSection = new RuneScapeGuideSection();
+    private final TutorialSection survivalSection = new SurvivalSection();
+    private final TutorialSection cookingSection = new CookingSection();
+    private final TutorialSection questSection = new QuestSection();
+    private final TutorialSection miningSection = new MiningSection();
+    private final TutorialSection fightingSection = new FightingSection();
+    private final TutorialSection bankSection = new BankSection();
+    private final TutorialSection priestSection = new PriestSection();
+    private final TutorialSection wizardSection = new WizardSection();
+
+    @Override
+    public void onStart() throws InterruptedException {
+        rsGuideSection.exchangeContext(getBot());
+        survivalSection.exchangeContext(getBot());
+        cookingSection.exchangeContext(getBot());
+        questSection.exchangeContext(getBot());
+        miningSection.exchangeContext(getBot());
+        fightingSection.exchangeContext(getBot());
+        bankSection.exchangeContext(getBot());
+        priestSection.exchangeContext(getBot());
+        wizardSection.exchangeContext(getBot());
+    }
+
+    @Override
+    public final int onLoop() throws InterruptedException {
+        if (isTutorialIslandCompleted()) {
+            Looter looter = new Looter();
+            looter.exchangeContext(getBot());
+            looter.onStart();
+            return 0;
         }
-		return random(200, 300);
-	}
-	
-	private void getScript() throws InterruptedException {
-		if(tutoNeeded()) {
-			needATask = false;
-			startTuto();
-		}
-	    if(!tutoNeeded()) {
-	    	needATask = false;
-	    	startLooter();
-	    }
-	}
-	
-	private void startTuto() throws InterruptedException {
-		TutorialIsland tuto = new TutorialIsland();
-		tuto.exchangeContext(getBot());
-		tuto.onStart();
-	}
-	
-	private void startLooter() throws InterruptedException {
-		Looter looter = new Looter();
-		looter.exchangeContext(getBot());
-		looter.onStart();
-	}
-	
-	private boolean tutoNeeded() {
-		return TUTO_ISLAND.contains(myPosition());
-	}
+
+        switch (getTutorialSection()) {
+            case 0:
+            case 1:
+                rsGuideSection.onLoop();
+                break;
+            case 2:
+            case 3:
+                survivalSection.onLoop();
+                break;
+            case 4:
+            case 5:
+                cookingSection.onLoop();
+                break;
+            case 6:
+            case 7:
+                questSection.onLoop();
+                break;
+            case 8:
+            case 9:
+                miningSection.onLoop();
+                break;
+            case 10:
+            case 11:
+            case 12:
+                fightingSection.onLoop();
+                break;
+            case 14:
+            case 15:
+                bankSection.onLoop();
+                break;
+            case 16:
+            case 17:
+                priestSection.onLoop();
+                break;
+            case 18:
+            case 19:
+            case 20:
+                wizardSection.onLoop();
+                break;
+        }
+        return 200;
+    }
+
+    private int getTutorialSection() {
+        return getConfigs().get(406);
+    }
+
+    private boolean isTutorialIslandCompleted() {
+        return getConfigs().get(281) == 1000 && myPlayer().isVisible();
+    }
 }
